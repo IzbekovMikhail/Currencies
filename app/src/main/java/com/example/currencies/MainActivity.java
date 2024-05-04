629018db0e5154c039b9371d55b0c6b1c7c87a1f
russiankizune <moonlightcatg@gmail.com>
2024-05-04 11:50:10 +0900

    04.05.2024 This is not done!

--- app/src/main/java/com/example/currencies/MainActivity.java
+++ app/src/main/java/com/example/currencies/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.currencies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,28 +17,16 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView)findViewById(R.id.listViews);
 
         ArrayList<DataFlags> list = new ArrayList<>();
-        list.clear();
 
+        list.add(new DataFlags(R.drawable.brazil_flag, R.string.brazil, R.string.brazilBRL));
+        list.add(new DataFlags(R.drawable.china_flag, R.string.china, R.string.chinaCNY));
+        list.add(new DataFlags(R.drawable.japan_flag, R.string.japan, R.string.japanJPY));
         list.add(new DataFlags(R.drawable.russia_flag, R.string.russian, R.string.russianRUB));
-        list.add(new DataFlags(R.drawable.south_africa_flag, R.string.south_africa, R.string.south_africaZAR));
         list.add(new DataFlags(R.drawable.singapore_flag, R.string.singapore, R.string.singaporeSGD));
+        list.add(new DataFlags(R.drawable.south_africa_flag, R.string.south_africa, R.string.south_africaZAR));
         list.add(new DataFlags(R.drawable.turkey_flag, R.string.turkish, R.string.turkishTRY));
-        list.add(new DataFlags(R.drawable.usa_flag, R.string.usa, R.string.usaUSD));
-        list.add(new DataFlags(R.drawable._flag, R.string.russian, R.string.russianRUB));
-        list.add(new DataFlags(R.drawable.china_flag, R.string.china, R.string.china));
 
         MyListAdapter myListAdapter = new MyListAdapter(this, list);
         listView.setAdapter(myListAdapter);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/currencies/MyListAdapter.java
+++ app/src/main/java/com/example/currencies/MyListAdapter.java
@@ -1,4 +1,70 @@
 package com.example.currencies;
 
-public class MyListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MyListAdapter extends BaseAdapter {
+    private LayoutInflater inflater;
+    private ArrayList<DataFlags> list;
+
+    public MyListAdapter(Context context, ArrayList<DataFlags> data) {
+        list = data;
+        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public DataFlags getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View v, ViewGroup parent) {
+        ViewHolder holder;
+        View convertView = v;
+
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.list_item, parent, false);
+            holder.flag = (ImageView)convertView.findViewById(R.id.flagImage);
+            holder.name = (TextView)convertView.findViewById(R.id.name);
+            holder.abbreviation = (TextView)convertView.findViewById(R.id.abbreviation);
+            convertView.setTag(holder);
+        }
+
+        holder = (ViewHolder)convertView.getTag();
+        DataFlags dataFlags = getData(position);
+
+        holder.flag.setImageResource(dataFlags.getFlagID());
+        holder.name.setText(convertView.getResources().getString(dataFlags.getNameID()));
+        holder.abbreviation.setText(convertView.getResources().getString(dataFlags.getAbbreviationID()));
+
+        return convertView;
+    }
+
+    DataFlags getData(int position) {
+        return getItem(position);
+    }
+
+    private static class ViewHolder {
+        private ImageView flag;
+        private TextView name;
+        private TextView abbreviation;
+    }
 }
